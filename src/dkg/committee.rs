c38a61a518e50f6b86df7ad35d71efbacc3c48f4
iquerejeta <querejeta.azurmendi@iohk.io>
2021-08-27 17:50:43 +0200

    test phase 4

--- src/dkg/committee.rs
+++ src/dkg/committee.rs
@@ -482,7 +482,7 @@ mod tests {
         let (_m3, broad_3) =
             DistributedKeyGeneration::<RistrettoPoint>::init(&mut rng, &environment, &mc3, &mc, 3);
 
-        // Now, party one fetches invalid state of a single party, mainly party two
+        // Now, party one fetches invalid state of a single party, mainly party three
         let fetched_state = vec![
             MembersFetchedState1 {
                 sender_index: 2,
@@ -524,5 +524,84 @@ mod tests {
         let (phase_3, _broadcast_data_3) = unwrapped_phase.to_phase_3(&[bd]);
         assert!(phase_3.is_ok());
         assert_eq!(phase_3.unwrap().state.qualified_set, [1, 1, 0])
+    }
+
+    #[test]
+    fn phase_4_tests() {
+        let mut rng = OsRng;
+
+        let mut shared_string = b"Example of a shared string.".to_owned();
+        let h = CommitmentKey::<RistrettoPoint>::generate(&mut shared_string);
+
+        let threshold = 2;
+        let nr_members = 3;
+        let environment = Environment::init(threshold, nr_members, h);
+
+        let mc1 = MemberCommunicationKey::<RistrettoPoint>::new(&mut rng);
+        let mc2 = MemberCommunicationKey::<RistrettoPoint>::new(&mut rng);
+        let mc3 = MemberCommunicationKey::<RistrettoPoint>::new(&mut rng);
+        let mc = [mc1.to_public(), mc2.to_public(), mc3.to_public()];
+
+        let (m1, broad_1) =
+            DistributedKeyGeneration::<RistrettoPoint>::init(&mut rng, &environment, &mc1, &mc, 1);
+        let (m2, broad_2) =
+            DistributedKeyGeneration::<RistrettoPoint>::init(&mut rng, &environment, &mc2, &mc, 2);
+        let (_m3, broad_3) =
+            DistributedKeyGeneration::<RistrettoPoint>::init(&mut rng, &environment, &mc3, &mc, 3);
+
+        // Fetched state of party 1
+        let fetched_state_1 = vec![
+            MembersFetchedState1 {
+                sender_index: 2,
+                indexed_shares: broad_2.encrypted_shares[0].clone(),
+                committed_coeffs: broad_2.committed_coefficients.clone(),
+            },
+            MembersFetchedState1 {
+                sender_index: 3,
+                indexed_shares: broad_3.encrypted_shares[0].clone(),
+                committed_coeffs: broad_3.committed_coefficients.clone(),
+            },
+        ];
+
+        // Fetched state of party 2
+        let fetched_state_2 = vec![
+            MembersFetchedState1 {
+                sender_index: 1,
+                indexed_shares: broad_1.encrypted_shares[0].clone(),
+                committed_coeffs: broad_1.committed_coefficients.clone(),
+            },
+            MembersFetchedState1 {
+                sender_index: 3,
+                indexed_shares: broad_3.encrypted_shares[1].clone(),
+                committed_coeffs: broad_3.committed_coefficients.clone(),
+            },
+        ];
+
+        // Now we proceed to phase two.
+        let (party_1_phase_2, _party_1_phase_2_broadcast_data) = m1.to_phase_2(&environment, &fetched_state_1, &mut rng);
+        let (party_2_phase_2, _party_2_phase_2_broadcast_data) = m2.to_phase_2(&environment, &fetched_state_2, &mut rng);
+
+        assert!(party_1_phase_2.is_ok());
+        assert!(party_2_phase_2.is_ok());
+
+        // We proceed to phase three
+        let (party_1_phase_3, _party_1_broadcast_data_3) = party_1_phase_2.unwrap().to_phase_3(&[]);
+        let (party_2_phase_3, party_2_broadcast_data_3) = party_2_phase_2.unwrap().to_phase_3(&[]);
+
+        assert!(party_1_phase_3.is_ok() && party_2_phase_3.is_ok());
+
+        // Fetched state of party 1. We have mimic'ed that party three stopped participating.
+        let fetched_state_phase_3 = vec![
+            MembersFetchedState3 {
+                sender_index: 2,
+                committed_coefficients: party_2_broadcast_data_3.unwrap().committed_coefficients
+            }
+        ];
+
+        // The protocol should finalise, given that we have two honest parties finalising the protocol
+        // which is higher than the threshold
+        assert!(party_1_phase_3.unwrap().to_phase_4(&fetched_state_phase_3).0.is_ok());
+
+        // todo: check that complaints are nicely covered (protocol aborts, and complains are valid)
     }
 }
